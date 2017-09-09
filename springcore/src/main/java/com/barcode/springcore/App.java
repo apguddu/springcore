e9be228ce45eb886f959393173dde721e6df18bb
Abhilash <apguddu@gmail.com>
2017-09-09 13:59:38 +0530

    initial push

---
+++ springcore/src/main/java/com/barcode/springcore/App.java
@@ -0,0 +1,13 @@
+package com.barcode.springcore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
