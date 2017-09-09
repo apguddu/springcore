e2d987492d6559198f0d719da39d3e007cb867f7
Abhilash <apguddu@gmail.com>
2017-09-09 14:10:18 +0530

    New Student class creation

---
+++ springcore/src/main/java/com/barcode/springcore/Student.java
@@ -0,0 +1,13 @@
+package com.barcode.springcore;
+
+public class Student {
+	
+     private int id; 
+     private String name;
+     
+     public void displayStudentInfo()
+     {
+    	 System.out.println("name and id -->> " + id + " " + name);
+     }
+
+}
